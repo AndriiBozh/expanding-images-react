7d16248c2d2189a52f0bad33923e4cf83cb4b3df
AndriiBozh <deux_jours_avant@hotmail.com>
2021-03-27 19:34:35 +0200

    Create images.js

---
+++ images.js
@@ -0,0 +1,24 @@
+const imagesUrls = [
+  {
+    image_url:
+      "https://images.unsplash.com/photo-1615648244238-3907d7c570dc?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80",
+  },
+  {
+    image_url:
+      "https://images.unsplash.com/photo-1515023404795-fe51df454877?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80",
+  },
+  {
+    image_url:
+      "https://images.unsplash.com/flagged/photo-1570700005880-4ecdb8595d4a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80",
+  },
+  {
+    image_url:
+      "https://images.unsplash.com/photo-1485586752437-03021cfb7ea4?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80",
+  },
+  {
+    image_url:
+      "https://images.unsplash.com/photo-1530527375905-701d92682317?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80",
+  },
+];
+
+export default imagesUrls;

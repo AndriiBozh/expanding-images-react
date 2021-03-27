48d29b2843988ea6047f5549fa1af1149d50286a
AndriiBozh <deux_jours_avant@hotmail.com>
2021-03-27 19:35:12 +0200

    Create imageTitles.js

---
+++ imageTitles.js
@@ -0,0 +1,9 @@
+const imageTitles = [
+  { title: "Playing with hair" },
+  { title: "Behind the bag" },
+  { title: "Girl with a book" },
+  { title: "Wearing a towel" },
+  { title: "Knees in a sand" },
+];
+
+export default imageTitles;
